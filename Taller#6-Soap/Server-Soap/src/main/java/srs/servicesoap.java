468f6fa333845094074a451d9e860f9a8c45fc26
Santiago Andres Del Valle Pinilla <santiagoandresdelvallepinilla@gmail.com>
2023-10-24 21:44:05 -0500

    Last features

--- Taller#6-Soap/Server-Soap/src/main/java/srs/servicesoap.java
+++ Taller#6-Soap/Server-Soap/src/main/java/srs/servicesoap.java
@@ -7,24 +7,24 @@ import java.util.List;
 
 @WebService
 public class servicesoap {
-    static List<cancion> listaCanciones = new ArrayList<>(
+    static List<srs.cancion> listaCanciones = new ArrayList<>(
             Arrays.asList(
-                    new cancion("Bohemian Rhapsody", "Queen", "1975"),
-                    new cancion("Imagine", "John Lennon", "1971"),
-                    new cancion("Thriller", "Michael Jackson", "1982"),
-                    new cancion("Billie Jean", "Michael Jackson", "1983"),
-                    new cancion("Like a Rolling Stone", "Bob Dylan", "1965"),
-                    new cancion("Hey Jude", "The Beatles", "1968"),
-                    new cancion("Boogie Wonderland", "Earth, Wind & Fire", "1979"),
-                    new cancion("Hotel California", "Eagles", "1976"),
-                    new cancion("Purple Haze", "Jimi Hendrix", "1967"),
-                    new cancion("Stairway to Heaven", "Led Zeppelin", "1971")
+                    new srs.cancion("Bohemian Rhapsody", "Queen", "1975"),
+                    new srs.cancion("Imagine", "John Lennon", "1971"),
+                    new srs.cancion("Thriller", "Michael Jackson", "1982"),
+                    new srs.cancion("Billie Jean", "Michael Jackson", "1983"),
+                    new srs.cancion("Like a Rolling Stone", "Bob Dylan", "1965"),
+                    new srs.cancion("Hey Jude", "The Beatles", "1968"),
+                    new srs.cancion("Boogie Wonderland", "Earth, Wind & Fire", "1979"),
+                    new srs.cancion("Hotel California", "Eagles", "1976"),
+                    new srs.cancion("Purple Haze", "Jimi Hendrix", "1967"),
+                    new srs.cancion("Stairway to Heaven", "Led Zeppelin", "1971")
             )
     );
     @WebMethod
-    public cancion busqueda(String nombre){
-        cancion retorno = new cancion();
-        for(cancion p: listaCanciones){
+    public srs.cancion busqueda(String nombre){
+        srs.cancion retorno = new srs.cancion();
+        for(srs.cancion p: listaCanciones){
             if(nombre.toLowerCase().equals(p.getNombre().trim().toLowerCase())){
                 retorno = p;
                 break;
@@ -33,9 +33,9 @@ public class servicesoap {
         return retorno;
     }
     @WebMethod
-    public List<cancion> obtener_todo(){
-        ArrayList<cancion> data = new ArrayList<>();
-        for(cancion p: listaCanciones){
+    public List<srs.cancion> obtener_todo(){
+        ArrayList<srs.cancion> data = new ArrayList<>();
+        for(srs.cancion p: listaCanciones){
             data.add(p);
         }
         return data;
